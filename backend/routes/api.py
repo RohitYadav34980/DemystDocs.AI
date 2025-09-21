f82c8e403c28149144f88f082d0b7695bc1893f9
RohitYadav34980 <RohitYadav34980@gmail.com>
2025-09-21 17:56:25 +0530

    fix: Update CORS middleware to allow all origins and rename OCR API endpoint to get_ocr

--- backend/routes/api.py
+++ backend/routes/api.py
@@ -33,7 +33,7 @@ else:
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"] if _wildcard else _origins,
+    allow_origins=["*"],
     allow_credentials=True,   
     allow_methods=["*"],
     allow_headers=["*"],
